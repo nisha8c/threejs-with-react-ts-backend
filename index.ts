dca6981461543ad8bdd8fa37d926e9a6fef4cfa3
Nisha Chavan <nisha.chavan@lekab.com>
2024-04-12 11:13:11 +0200

    getWeekMenus

--- index.ts
+++ index.ts
@@ -9,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
+const PORT = process.env.SERVER_PORT;
+
 // Define an asynchronous function for starting the server
 async function startServer() {
     // Wait for the database connection to be established
@@ -24,8 +26,8 @@ async function startServer() {
     app.get('/api/weekMenus/:id', getWeekMenuById);
 
 
-    app.listen(8000, () => {
-        console.log('Server is running on port 8000');
+    app.listen(PORT, () => {
+        console.log('Server is running on port : ', PORT);
     });
 }
 
